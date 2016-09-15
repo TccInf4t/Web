a81e3c9150f60fb7699734c824a019851fd4c5c5
MATEUS FERREIRA MORELLI <15160229@CFP127ALUNO.LOCAL>
2016-09-15 16:45:42 -0300

    Pagina Produto e promocao

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $(function(){
+	$("#btnPesquisa").click(function(){
+		$("#caixaPesq").css("display", "block");
+		$("#txtPesq").focus();
+		$("#caixaPesq").animate({
+			top: "70px",
+			opacity: "1"
+		}, 100);
+	});
+
+	$("#caixaPesqFechar").click(function(){
+		$("#caixaPesq").animate({
+			top: "80px",
+			opacity: "0"
+		}, 100, function(){
+			$("#caixaPesq").css("display", "none");	
+		});
+		
+	});
+
+	// Mostrando a barra de detalhes
 	$("#btnMenu").click(function(){
 		$("#barra_lateral").animate({
 			left: "0px"
 		}, 200);
 	});
 	
+	// Escondendo a barra de detalhes
 	$(document).click(function(event){
 		var obj = event.target;
 		
