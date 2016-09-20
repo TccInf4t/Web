e4caeac16b783db5900946af7f0aba0c90b1c9ce
P2S <P2S>
2016-09-20 10:41:00 -0300

    Ultimas alterações (Mateus)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(function(){
+	$(".caixa").click(function(){
+		window.location = "produto.html";
+	});
+
+	// Caixa de pesquisa
 	$("#btnPesquisa").click(function(){
 		$("#caixaPesq").css("display", "block");
 		$("#txtPesq").focus();
@@ -35,4 +40,42 @@ $(function(){
 			}, 200);
 		}
 	});
+
+	// Slider
+	$('#slides').cycle({
+		fx: 'fade',
+		pause: 1,
+		prev: '#btnEsquerdo',
+		next: '#btnDireito'
+	});
 });
+
+// function slide(){
+// 	var width = 1000;
+// 	var vel = 200;
+// 	var slides = $("#slides");
+// 	var slideAtual = 1;
+
+// 	$("#btnEsquerdo").click(anteriorSlide);
+// 	$("#btnDireito").click(proximoSlide);
+
+// 	function proximoSlide(){
+// 		$("#slides").animate({
+// 			left: "-=" + width + "px"
+// 		}, vel, function(){
+// 			slideAtual ++;
+
+			
+// 			if(slideAtual === $("#slides").children().length + 1){
+// 				slideAtual = 1;
+// 				slides.css("left", "0");
+// 			}
+// 		});
+// 	}
+
+// 	function anteriorSlide(){
+// 		$("#slides").animate({
+// 			left: "+=" + width + "px"
+// 		}, vel);	
+// 	}
+// }
